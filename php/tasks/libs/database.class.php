ec8f1e6931771e6dfdd2c67f852b33046ec218f4
Paul Lockyer <plockyer@googlemail.com>
2019-01-04 10:57:26 +0000

    Start of something great

---
+++ php/tasks/libs/database.class.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     *	Filename: database.class.php
+     *      Desc:  Database functionality class
+     *      Author: Paul Lockyer
+     *      Contact: plockyer@googlemail.com
+     */
+
+    class DatabaseObject{
+    }
+
+?>
