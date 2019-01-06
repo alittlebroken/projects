7c6c360c8c6c3ac57a357044938990478c4aecb2
Paul Lockyer <plockyer@googlemail.com>
2019-01-06 08:08:41 +0000

    Create bootstrap.php

---
+++ php/tasks/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Application bootstrap file
+*
+* Use this to crete all the reuired objects and include common settings etc
+*
+*/
+
+/**
+* Include configuration for application
+*
+*/
+include "/config/configuration.php";
+
+?>
