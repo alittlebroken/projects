1b5b3894f7de283804c2ab4114b07c59a4cc010e
Paul Lockyer <plockyer@googlemail.com>
2019-01-06 07:46:20 +0000

    Created initial version

---
+++ php/tasks/config/configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* Database configuration
+*
+*/
+define("DB_SERVER","localhost");
+define("DB_NAME","projects");
+define("DB_UID","projectdev");
+define("DB_UID_PWD","jshsdjkfhsfhsjk");
+
+?>
