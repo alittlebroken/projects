34fa89381289c4e7c4e1f663723677666cef5696
Paul Lockyer <plockyer@googlemail.com>
2019-01-06 08:03:47 +0000

    Create header.php

---
+++ php/tasks/public/header.php
@@ -0,0 +1,17 @@
+<?php
+
+/** 
+* Site header file
+*
+*/
+
+/**
+* Include the bootstrap file to get the party started
+*/
+include "../bootstrap.php";
+
+?>
+<!DOCTYPE html>
+    <head lang="en">
+        <meta charset="UTF-8">
+    </head>
