570b8a52c879560d155e8dd99edb9ee75947ba5a
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 08:02:31 +0000

    Create comments.php

---
+++ php/basics/comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Comments</title>
+    </head>
+    <body>
+      <h2>PHP Comments Example</h2>
+      Best to look at source code to see them
+       
+        <?php
+        
+           // Single line comment
+           
+           # Another single line comment
+           
+           /*
+               Multi line comment
+           */
+        
+        ?>
+    
+    </body>
+</html>
