02a4c954f9e3988a345d24aa01898583c5f4ae2f
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 16:25:32 +0000

    Create constants.php

---
+++ php/basics/constants.php
@@ -0,0 +1,16 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Constants</title>
+    </head>
+    <body>
+    
+        <h2>PHP Constants</h2>
+    
+    </body>
+</html>
+<?php
+?>
