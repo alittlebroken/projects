fb35bd745364e7f84a6739f3e84ca73c9c152819
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 08:05:50 +0000

    Create skeleton.php

---
+++ php/skeleton.php
@@ -0,0 +1,17 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP & HTML 5 Skeleton Document</title>
+    </head>
+    <body>
+    
+    
+    </body>
+</html>
+<?php
+
+?>
