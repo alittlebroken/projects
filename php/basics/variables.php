82233a655a0a8b2be5b29e87162b6cbd7bfa9b7c
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 09:14:34 +0000

    Update variables.php

--- php/basics/variables.php
+++ php/basics/variables.php
@@ -11,6 +11,12 @@ $output_string = "Hello world!";
 $output_string_alpha = "Hello";
 $output_string_beat = " world!";
 
+/*
+* Variables to hold numbers
+*/
+$output_num_alpha = 5;
+$output_num_beta = 10;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,6 +39,22 @@ $output_string_beat = " world!";
           echo "All together: $output_string, $output_string_alpha, $output_string_beta<br>";
       ?>
     
+        <h2>Variables - Numbers</h2>
+        <hr>
+        <?php
+        
+        // Display the numbers to start with
+        echo "First Number: " . $output_num_alpha . "<br>";
+        echo "Second Number: " . $output_num_beta . "<br>";
+        echo "<br>";
+        
+        // Now perform some math on the variables
+        echo "Numbers added together: " . $output_num_alpha + $output_num_beta . "<br>";
+        echo "Numbers multiplied together: " . $output_num_alpha * $output_num_beta . "<br>";
+        echo "Numbers divided together: " $output_num_alpha / $output_num_beta . "<br>";
+            
+        ?>
+        
     </body>
 </html>
 <?php
