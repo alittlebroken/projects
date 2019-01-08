971d6911ca50f19b6c9355e5ef0fdb6c96acb40f
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 11:30:06 +0000

    Update variables.php

--- php/basics/variables.php
+++ php/basics/variables.php
@@ -17,6 +17,57 @@ $output_string_beat = " world!";
 $output_num_alpha = 5;
 $output_num_beta = 10;
 
+/*
+* Variable to test scope - this var is a global var and can be accessed oanywhere in the script that is outside of functions
+*/
+$output_global_var = "Paul Lockyer";
+
+/* 
+* This function tests global and local scope
+*/
+function test_var_scope()
+{
+ 
+    // Print out the global var, produces an error as we can't access it
+    echo "Global Var: $output_global_var";
+    
+    // Create a local scope var
+    $output_local_var = "Simon Lockyer";
+    
+    echo "Local var (Inside function): $output_local_var";
+    
+}
+
+/*
+* This function references the global var correctly
+*/
+function test_var_scope_fixed()
+{
+    // Refrence the global var
+    global $output_global_var;
+    
+    // Print out the global var, this time works as we have put a globla reference to it
+    echo "Global Var: $output_global_var";
+    
+    // Create a local scope var
+    $output_local_var = "Sue Lockyer";
+    
+    echo "Local var (Inside function): $output_local_var";
+}
+
+// Another way a function can access the global var without the global keyword
+function test_var_scope_new()
+{
+ 
+    echo "Global var access via GLOBAL: " . $GLOBALS["output_global_var"] . "<br>";
+    
+    // Create aniother locally scoped var
+    $output_local_var = "Glen Lockyer";
+    
+    echo "Local var inside function: $output_local_var <br>";
+    
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,7 +77,7 @@ $output_num_beta = 10;
     </head>
     <body>
     
-      <h2>Variables - String & Strinf Concatination</h2>
+      <h2>Variables - String & String Concatination</h2>
       <hr>
       <?php
       
@@ -53,6 +104,26 @@ $output_num_beta = 10;
         echo "Numbers multiplied together: " . $output_num_alpha * $output_num_beta . "<br>";
         echo "Numbers divided together: " $output_num_alpha / $output_num_beta . "<br>";
             
+        ?>
+        
+        <h2> Variables  - Scope </h2>
+        <hr>
+        <?php
+        
+            echo "Global scope var: " . $output_global_var . "<br>";
+        
+            // Attempt to print out the global var inside the function, should error at this point
+            echo "Global and Local inside function (FAIL): " . test_var_scope() . "<br>";
+        
+           // Attempt to print the local var inside ther function
+           echo "Local var inside function (FAIL): " . $output_local_var ."<br>";
+        
+           // Now attempt to print out the global var from within the new function
+           echo "Global and local inside function (Fixed): test_var_scope_fixed() <br>";
+        
+           // Now output the global var another way
+           echo "Access global var in fucntion via GLOBAL: test_var_scope_new() <br>";
+        
         ?>
         
     </body>
