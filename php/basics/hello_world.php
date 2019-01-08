d5c399f78fc3ef7c9cfc21cd6633ca739e1ee42f
Paul Lockyer <plockyer@googlemail.com>
2019-01-08 07:52:46 +0000

    Create hello_world.php

---
+++ php/basics/hello_world.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello World PHP Edition</title>
+    </head>
+    <body>
+
+    <h3>
+        <?php echo "Hello World!"; ?>
+    </h3>
+
+    </body>
+</html>
