8b4d3cabd276efb07582e2aa86b15e5653f17497
Paul Lockyer <plockyer@googlemail.com>
2019-01-11 15:53:49 +0000

    Create ifelse.php

---
+++ php/basics/ifelse.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*
+*  Create some vars to test the if else statement with
+*/
+$var_current_hour = 8;
+$var_morning_cutoff = 12;
+$var_afternoon_cutoff = 18;
+$var_evening_cutoff = 9;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP IF ELSE</title>
+    </head>
+    <body>
+        <h2>PHP IF ELSE Statements</h2>
+
+        <?php
+            
+            echo "Curent time of day: " . $var_current_hour . "<br>";
+            
+            
+        ?>
+
+    </body>
+</html>
+<?php
+?>
