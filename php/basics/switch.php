4dea2c4115bdf0d4a70f1d57bb5e35dbd5b3b49f
Paul Lockyer <plockyer@googlemail.com>
2019-01-14 17:04:52 +0000

    Create switch.php

---
+++ php/basics/switch.php
@@ -0,0 +1,17 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Switch</title>
+    </head>
+    <body>
+        <h2>PHP Switches</h2>
+        <?php
+        
+        ?>    
+    </body>
+</html>
+<?php
+?>
