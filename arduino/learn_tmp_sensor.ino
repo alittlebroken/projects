101c3882198cfd286af0bc58c500c6d5a9e70c2e
Paul Lockyer <plockyer@googlemail.com>
2019-01-16 09:52:49 +0000

    Create learn_tmp_sensor.ino

---
+++ arduino/learn_tmp_sensor.ino
@@ -0,0 +1,22 @@
+/*
+
+   learn_tmp_sensor.ino
+   Paul Lockyer (plockyer@googlemail.com)
+   2019-01-16
+   
+   Script to read the current tempertaure from a sensor every few seconds
+
+*/
+
+/*
+    Which port is the temp sensor attached to
+*/
+int sensorTempPort = A0;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
