d38a0c9238b041482da6c7576ab7865d782b1147
Paul Lockyer <plockyer@googlemail.com>
2019-01-21 08:55:19 +0000

    Update switch.php

--- php/basics/switch.php
+++ php/basics/switch.php
@@ -1,4 +1,19 @@
 <?php 
+
+/*
+    switch.php
+    Demostrates the use of switches in PHP
+    Paul Lockyer (plockyer@googlemail.com)
+    January 21st 2019
+*/
+
+/*
+
+    Variable to perform switch test on
+
+*/
+$favourite_colour = "blue";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +24,34 @@
     <body>
         <h2>PHP Switches</h2>
         <?php
+        
+        /*
+            Test the users favourite colour and using the switch statement display it to the screen
+            
+        */
+        switch ($favourite_colour) {
+            case "blue":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            case "red":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            case "yellow":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            case "black":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            case "green":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            case "purple":
+                echo "Your favourite colour is " . $favourite_colour . "<br>";
+                break;
+            default:
+                echo "Hmm it seems your favourite colour does not exist yet.<br>";
+        }
+        
         
         ?>    
     </body>
