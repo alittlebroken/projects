7f0c9d74a2fd3e8e9910d25c994444ddb58cd291
Paul Lockyer <plockyer@googlemail.com>
2019-01-25 16:16:19 +0000

    Create phpbasics_setup.sql

---
+++ php/basics/phpbasics_setup.sql
@@ -0,0 +1,20 @@
+# MariaDB/MySql Database setup scripts
+# phpbasics_setup.sql
+# Paul Lockyer ( plockyer@googlemail.com)
+# 2019-01-25
+# Version 1
+# Script to setup the database and tables needed to practice my php skills
+
+# Database creation
+create or replace database php_basics character set 'utf8mb4' collate 'utf8mb4_unicode_ci';
+
+# Create a table to allow us to practice selecting, adding and deleting into
+# Just a simple contacts list:
+# Name, phone number, country and country code
+create or replace table if not exists contacts(
+    contact_id int not null auto_increment primary key,
+    forename varchar(100) not null,
+    lastname varchar(150) not null,
+    telephone_num varchar(20) not null,
+    country_area_code int not null
+);
