8c6068d615f023eb87cdb8495a6d8785aa15a8f6
Paul Lockyer <plockyer@googlemail.com>
2019-02-01 08:53:41 +0000

    Create while.php

---
+++ php/basics/while.php
@@ -0,0 +1,26 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP While Loop</title>
+    </head>
+    <body>
+        <h1>PHP While Loop</h1>
+        <?php
+          
+          /*
+          *   Using a while loop, iterate through some numbers
+          */
+          while($x < 10)
+          {
+              echo "The number is: " . $x . "<br>";
+              $x++;
+          }
+          
+        ?>
+    </body>
+</html>
+<?php
+?>
