87d4775c7529ab23c4f15a4a4061c8fe417cbf3b
Paul Lockyer <plockyer@googlemail.com>
2019-02-04 07:47:12 +0000

    Update for.php

--- php/basics/for.php
+++ php/basics/for.php
@@ -1,1 +1,32 @@
+<?php 
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP For Loops</title>
+    </head>
+    <body>
+      <h1>PFP For Loop</h1>
+      <?php
+      
+        for($i=1;$i < 11;$i++)
+        {
+         
+          echo "The number in the for loop is: " . $i . "<br>";
+          
+        }
+      
+      ?>
+       <br>
+      <?php
+      
+          $fruits = arrary();
+      
+      ?>
+    </body>
+</html>
+<?php
+
+?>
