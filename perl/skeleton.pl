0175f5409fa11ff60a2b75226a0954c08ef1f469
Paul Lockyer <plockyer@googlemail.com>
2019-02-13 10:11:19 +0000

    Create skeleton.pl

---
+++ perl/skeleton.pl
@@ -0,0 +1,7 @@
+#/usr/bin/env perl
+
+# File:          skeleton.pl
+# Author:        Paul Lockyer
+# Version:       1
+# Description:   Perl Skeleton script contain the most basic and common code that my scripts will need
+#                Should save me some time
