7928210090ac65fff7813c602d04f6ba1dc06497
Paul Lockyer <plockyer@googlemail.com>
2019-02-22 15:43:53 +0000

    Create core.js

---
+++ javascript/rpglite/core.js
@@ -0,0 +1,24 @@
+let player = {
+    stats = {
+        status: "alive",
+        health: 100,
+        mana: 100,
+        stamina: 100,
+        money: 0,
+        experience: 0,
+        times_died: 0,
+        monsters_killed: 0
+    },
+    skillsets = {
+        mining: 0,
+        forestry: 0,
+        herbalism: 0,
+        tanning: 0,
+        blacksmithing: 0,
+        woodworking: 0,
+        alchemy: 0,
+        fishing: 0,
+        tailoring: 0,
+        leatherworking: 0
+    }
+};
