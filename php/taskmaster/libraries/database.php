ee6362f2d67691c8bef5768669c484015b99ef7b
Paul Lockyer <plockyer@googlemail.com>
2019-03-06 17:07:06 +0000

    Create database.php

---
+++ php/taskmaster/libraries/database.php
@@ -0,0 +1,30 @@
+<?php
+
+   /**
+   *    name:         database.php
+   *    description:  Provides a connection to a database for the apps models
+   *    auth:         Paul Lockyer
+   *    created:      6th March 2019
+   *    version:      0.1
+   **/
+   
+   /**
+   *     properties to allow a connection to the database, fairly self explanatory
+   *    on what each property holds
+   **/
+   $db_name = "taskmaster";
+   $db_host = "localhost";
+   $db_uid  = "tskmstusr";
+   $db_pass = "Gr34t56IhGffT";
+   
+   /**
+   *    Connect to the database with the above parameters and don't forget to do this within
+   *    a try catch block to catch any nasty exception we might encounter
+   **/
+   try{
+   
+   }catch(){
+   
+   }
+
+?>
