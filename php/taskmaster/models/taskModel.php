7e7d65965428145d0180a630c0fecd031eded23e
Paul Lockyer <plockyer@googlemail.com>
2019-03-07 09:31:19 +0000

    Create taskModel.php

---
+++ php/taskmaster/models/taskModel.php
@@ -0,0 +1,44 @@
+<?php
+    /**
+    *    name:        taskModel.php
+    *    Description: Model that handles the logic for the application to
+    *                 work with the tasks module.
+    *    author:      Paul Lockyer
+    *    created:     7th March 2019
+    *    version:     0.1
+    **/
+    
+   /**
+   *     class taskModel
+   *     
+   *     All properties and methods used to work with the tasks module
+   *
+   **/
+   class taskModel{
+   
+        /**
+        *  holds the internal reference to the database connection
+        **/
+        protected $db;
+    
+    
+       /** 
+       * Standard class constructor, takes in the database reference.
+       **/
+       public function __construct($db)
+       {
+           $this->db = $db;
+       }
+      
+       /**
+       * Fuction that returns all the rows from the task table
+       **/
+       public function getTasks()
+       {
+           
+           
+       }
+   
+   }
+
+?>
