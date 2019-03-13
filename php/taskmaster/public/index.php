06040bdc5201fc8f459a242497e8185f7a7c49a3
Paul Lockyer <plockyer@googlemail.com>
2019-03-13 17:58:39 +0000

    Create index.php

---
+++ php/taskmaster/public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    /**
+    * Now include an app based on what has been selected, otherwise just load the front page
+    **/
+    switch($_GET["app"]{
+     case "tasks":
+       include("../models
+    }
+
+?>
