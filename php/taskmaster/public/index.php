083b5df6d7e146658bc22a1f22bb54f7d33f4356
Paul Lockyer <plockyer@googlemail.com>
2019-03-15 08:26:27 +0000

    Update index.php

--- php/taskmaster/public/index.php
+++ php/taskmaster/public/index.php
@@ -1,11 +1,20 @@
 <?php
 
     /**
+    * Load the application bootstrap file, before anything else
+    **/
+    include("../bootstrap.php");
+
+    /**
     * Now include an app based on what has been selected, otherwise just load the front page
     **/
-    switch($_GET["app"]{
-     case "tasks":
-       include("../models
+    switch($_GET["app"])
+    {
+        case "tasks":
+            include("../models/tasks.php");
+            break;
+        default:
+            include("../models/default.php");
     }
 
 ?>
