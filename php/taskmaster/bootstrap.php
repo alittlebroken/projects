a3fc2a583dc955d511fb37293cf69a37ff59d3e6
Paul Lockyer <plockyer@googlemail.com>
2019-03-24 06:46:03 +0000

    Add custom auto register function

--- php/taskmaster/bootstrap.php
+++ php/taskmaster/bootstrap.php
@@ -15,6 +15,45 @@
    *//
    define("DS","/");
 
+   /**
+   * Assing and create the class autoloader 
+   **/
+   spl_autoload_register('app_loader');
+
+   function app_loader($classname)
+   {
+    
+       /** 
+       * Search through the local folders and check if the 
+       * class named required exists. Check the models
+       * folder first then views and finally the
+       * libraries folder
+       *
+       * class name is the name of the app I.E TaskMaster
+       **/
+       if(file_exists(APP_ROOT.DS."models".DS.$classname.DS.$classname.".model.php"))
+       {
+           include(APP_ROOT.DS."models".DS.$classname.DS.$classname.".model.php");
+       }
+       elseif(file_exists(APP_ROOT.DS."models".DS.$classname.".model.php"))
+       {
+           include(APP_ROOT.DS."models".DS.$classname.".model.php");
+       }
+       elseif(file_exists(APP_ROOT.DS."views".DS.$classname.DS.$classname.".view.php"))
+       {
+           include(APP_ROOT.DS."views".DS.$classname.DS.$classname.".view.php");
+       }
+       elseif(file_exists(APP_ROOT.DS."views".DS.$classname.".view.php"))
+       {
+           include(APP_ROOT.DS."views".DS.$classname.".view.php");
+       }
+       else(file_exists(APP_ROOT.DS."libraries".DS.$classname.".php"))
+       {
+           include(APP_ROOT.DS."libraries".DS.$classname.".php");
+       }
+       
+   }
+
     /**
     * Setup the database resource here and connect to it
     **/
