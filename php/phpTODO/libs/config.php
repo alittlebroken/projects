5d281193298eaf55837fe713a53fb91b366b10b4
Paul Lockyer <plockyer@googlemail.com>
2019-07-30 08:29:57 +0100

    Create config.php

---
+++ php/phpTODO/libs/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    ## config.php
+    ## Configuration options for this web application
+    ## P Lockyer ( plockyer@googlemail.com ) 2019-07-30
+    
+    ## Assign the values to an array and then return them
+    ## Don't forget to assign it to an array at include time
+    return = [
+        'database' => [
+         'hostname' => 'localhost',
+         'username' => 'phptodo',
+         'password' => 'G34ThYre6739'
+        ]
+    ];
+
+?>
