468db0a90f01b50d43bc86b2afc08bf694ef7afd
Paul Lockyer <plockyer@googlemail.com>
2019-07-30 08:31:51 +0100

    Create index.php

---
+++ php/phpTODO/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    ## index.php
+    ## Application front end
+    ## P Lockyer (plockyer@googlemail.com) 2019-07-30
+    
+    ## Include and assign the configuration
+    $config = include("/libs/config.php");
+    
+
+?>
