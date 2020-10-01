d3ab5df1d713b9aa12fedca0abe2c6155b372ab8
Paul Lockyer <plockyer@googlemail.com>
2020-10-01 17:01:57 +0100

    Create dice.py

---
+++ python/dice/dice.py
@@ -0,0 +1,16 @@
+// dice.py
+// A simple dice app for the terminal
+// P Lockyer ( plockyer@googlemail.com )
+// 2020-10-01
+
+// Imports
+import random as rand
+
+// Setup
+
+// Functions
+
+
+// Main Program
+if __name__ == "__main_":
+    pass
